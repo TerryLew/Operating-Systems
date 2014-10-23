85159ca2ba6e840e2799af1a19bf2598dfb58064
TerryLew <terrysatou@gmail.com>
2014-10-23 19:19:09 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
+int sum(int a, int b,int c) {
+  return a+b+c;
+}
+
 int main() {
   std::cout<<"123abc\n";
   
-  
+  int a = 1;
+  int b = 2;
+  int c = 3;
   std::cout << "It's me. Man" << std::endl;
-  std::cout << "It's second time." << std::endl;
+  int s = sum(a,b,c);
+  std::cout << "The sum is "<< s << std::endl;
+  
+  return 0;
 }
